78a419ff666f080a6a6e89a2a52334b9ace8f68c
Grzegorz Bokota <bokota+github@gmail.com>
2020-05-25 18:10:59 +0200

    add run script for new job

--- simple_run_v2.py
+++ simple_run_v2.py
@@ -1,12 +1,12 @@
 import argparse
 import os
 import glob
+import pickle
 
 import numpy as np
 import tensorflow as tf
 
 
-from neural_differential.normalisation_parameters import load_data, normalize_input, normalize_output
 
 
 def run_model(input_data, output_data, model_chk, epochs):
@@ -17,7 +17,7 @@ def run_model(input_data, output_data, model_chk, epochs):
     model = tf.keras.models.load_model(model_chk)
     history = model.fit(
         input_data,
-        output_data[:, : model.output_shape[1]],
+        output_data[:, model.output_shape[1]],
         epochs=epochs,
         validation_split=0.2,
         verbose=0,
@@ -37,12 +37,13 @@ def main():
     args = parser.parse_args()
     input_files = sorted(glob.glob(os.path.join(args.data_dir, "input*csv")))
     output_files = sorted(glob.glob(os.path.join(args.data_dir, "output*csv")))
-    norm_param = load_data(args.norm_parameters)
+    with open(args.norm_parameters, 'rb') as ff:
+        norm_param = pickle.load(ff)
     inputs = np.concatenate([np.loadtxt(file_name, delimiter=",") for file_name in input_files])
     outputs = np.concatenate([np.loadtxt(file_name, delimiter=",") for file_name in output_files])
-    inputs = normalize_input(norm_param, inputs)
-    outputs = normalize_output(norm_param, outputs)
-    run_model(inputs, outputs[:, 1:], args.model, args.epochs)
+    inputs = norm_param.normalize_input(norm_param, inputs)
+    outputs = norm_param.normalize_output(norm_param, outputs)
+    run_model(inputs, outputs, args.model, args.epochs)
 
 
 class EpochDots(tf.keras.callbacks.Callback):
